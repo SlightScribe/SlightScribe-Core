4c47272b548c9fedb00606434faf1045e03d2d35
James <james_baster@shelter.org.uk>
2016-10-24 16:14:25 +0100

    Admin can create new files

---
+++ src/SlightScribeBundle/Controller/AdminProjectVersionEditController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace SlightScribeBundle\Controller;
+
+use SlightScribeBundle\Entity\File;
+use SlightScribeBundle\Entity\Project;
+use SlightScribeBundle\Form\Type\AdminFileNewType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+/**
+ *  @license 3-clause BSD https://github.com/SlightScribe/SlightScribe-Core/blob/master/LICENSE.md
+ *  @link https://github.com/SlightScribe/SlightScribe-Core
+ */
+class AdminProjectVersionEditController extends AdminProjectVersionController
+{
+
+    protected function build($projectId, $versionId)
+    {
+        parent::build($projectId, $versionId);
+    }
+
+
+
+
+    public function newFileAction($projectId, $versionId, Request $request)
+    {
+        // build
+        $this->build($projectId, $versionId);
+        //data
+
+        $doctrine = $this->getDoctrine()->getManager();
+
+        $file = new File();
+        $file->setProjectVersion($this->projectVersion);
+
+        $form = $this->createForm(new AdminFileNewType(), $file);
+
+        if ($request->getMethod() == 'POST') {
+            $form->handleRequest($request);
+            if ($form->isValid()) {
+                $doctrine->persist($file);
+                $doctrine->flush();
+
+                return $this->redirect($this->generateUrl('slight_scribe_admin_project_version_file_show', array(
+                    'projectId'=>$this->project->getPublicId(),
+                    'versionId'=>$this->projectVersion->getPublicId(),
+                    'fileId'=>$file->getPublicId(),
+                )));
+            }
+        }
+
+
+        return $this->render('SlightScribeBundle:AdminProjectVersionEdit:newFile.html.twig', array(
+            'project' => $this->project,
+            'version' => $this->projectVersion,
+            'form' => $form->createView(),
+        ));
+    }
+
+
+
+}
---
+++ src/SlightScribeBundle/Form/Type/AdminFileNewType.php
@@ -0,0 +1,67 @@
+<?php
+
+
+namespace SlightScribeBundle\Form\Type;
+
+use SlightScribeBundle\Entity\LetterVersion;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackValidator;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ *  @license 3-clause BSD https://github.com/SlightScribe/SlightScribe-Core/blob/master/LICENSE.md
+ *  @link https://github.com/SlightScribe/SlightScribe-Core
+ */
+class AdminFileNewType extends AbstractType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+
+        $builder->add('titleAdmin', 'text', array(
+            'required' => true,
+            'label'=>'Title (For Admins)'
+        ));
+
+        // TODO enforce slug like!
+        // TODO A value is required here when it shouldn't be!
+        $builder->add('publicId', 'text', array(
+            'required' => false,
+            'label'=>'Key'
+        ));
+
+        $builder->add('filename', 'text', array(
+            'required' => true,
+            'label'=>'Filename'
+        ));
+
+
+        $builder->add('type', ChoiceType::class, array(
+            'choices_as_values' => true,
+            'choices'  => array(
+                'Text' => 'LETTER_TEXT',
+                'PDF' => 'LETTER_PDF',
+            ),
+        ));
+
+
+        $builder->add('letterContentTemplate', 'textarea', array(
+            'required' => false,
+            'label'=>'Letter Content'
+        ));
+
+    }
+
+    public function getName() {
+        return 'tree';
+    }
+
+
+    public function getDefaultOptions(array $options) {
+        return array(
+            'data_class' => 'SlightScribeBundle\Entity\File',
+        );
+    }
+
+}
