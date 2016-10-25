440da1bb232ff0c4ba8a5763ec6dbe424d670bb6
James <james_baster@shelter.org.uk>
2016-10-25 14:22:28 +0100

    Admin can see show page for IP block

---
+++ src/SlightScribeBundle/Controller/AdminBlockIPController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace SlightScribeBundle\Controller;
+
+use SlightScribeBundle\Entity\BlockIP;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ *  @license 3-clause BSD https://github.com/SlightScribe/SlightScribe-Core/blob/master/LICENSE.md
+ *  @link https://github.com/SlightScribe/SlightScribe-Core
+ */
+class AdminBlockIPController extends Controller
+{
+
+    /** @var  BlockIP */
+    protected $blockIP;
+
+    protected function build($blockId)
+    {
+        $doctrine = $this->getDoctrine()->getManager();
+        // load
+        $repository = $doctrine->getRepository('SlightScribeBundle:BlockIP');
+        $this->blockIP = $repository->findOneById($blockId);
+        if (!$this->blockIP) {
+            throw new  NotFoundHttpException('Not found');
+        }
+
+
+    }
+
+    public function indexAction($blockId)
+    {
+        // build
+        $this->build($blockId);
+        //data
+
+
+        return $this->render('SlightScribeBundle:AdminBlockIP:index.html.twig', array(
+            'blockIP' => $this->blockIP,
+        ));
+    }
+
+}
